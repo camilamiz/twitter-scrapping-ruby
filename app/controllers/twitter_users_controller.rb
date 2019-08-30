7c681ad95dd5511c79e0e7797c5ec5d6d3bc9226
camila mizumoto <camila.yuri.m@gmail.com>
2019-08-29 22:28:48 -0300

    Remove devise gem from app and add search feature

--- app/controllers/twitter_users_controller.rb
+++ app/controllers/twitter_users_controller.rb
@@ -2,26 +2,25 @@ require 'open-uri'
 require 'nokogiri'
 
 class TwitterUsersController < ApplicationController
-  def index
-    if params[:query].present?
-        sql_query = "name ILIKE :query OR page_address ILIKE :query"
-      @movies = Movie.where(sql_query, query: "%#{params[:query]}%")
-    else
-        @twitter_users = TwitterUser.all
+    def index
+        if params[:query].present?
+            sql_query = "name ILIKE :query OR page_address ILIKE :query"
+            @twitter_user = TwitterUser.where(sql_query, query: "%#{params[:query]}%").first
+        else
+            @twitter_users = TwitterUser.all
+        end
     end
-  end
   
-#   private
-  
-  def get_users(url)
-    # t_user = TwitterUser.where(name: name).first
-    html_file = open(url).read
-    # html_file = open(t_user.page_address).read
-    html_doc = Nokogiri::HTML(html_file)
+    private
+    def get_users(url)
+        # t_user = TwitterUser.where(name: name).first
+        html_file = open(url).read
+        # html_file = open(t_user.page_address).read
+        html_doc = Nokogiri::HTML(html_file)
 
-    puts html_doc.css('a.ProfileHeaderCard-nameLink.u-textInheritColor.js-nav').inner_text
-    puts html_doc.css('h2.ProfileHeaderCard-screenname.u-inlineBlock.u-dir').inner_text.strip
-    puts html_doc.css('p.ProfileHeaderCard-bio.u-dir').inner_text.strip
+        puts html_doc.css('a.ProfileHeaderCard-nameLink.u-textInheritColor.js-nav').inner_text
+        puts html_doc.css('h2.ProfileHeaderCard-screenname.u-inlineBlock.u-dir').inner_text.strip
+        puts html_doc.css('p.ProfileHeaderCard-bio.u-dir').inner_text.strip
     end
 
 end
