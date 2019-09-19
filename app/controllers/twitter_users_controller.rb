4b1b513375a96d0a97a3e4bb49d8d5f15708a6ea
camila mizumoto <camila.yuri.m@gmail.com>
2019-09-18 21:13:23 -0300

    Insert scrapping logic when creating a new user

--- app/controllers/twitter_users_controller.rb
+++ app/controllers/twitter_users_controller.rb
@@ -10,27 +10,40 @@ class TwitterUsersController < ApplicationController
             @twitter_users = TwitterUser.all
         end
     end
-
+    
     def new
         @twitter_user = TwitterUser.new
     end
-
+    
     def create
-        @twitter_user= TwitterUser.new
+        @twitter_user= TwitterUser.new(twitter_user_params)
+        url = @twitter_user.page_address
+        html_doc = get_user(url)
+        @twitter_user.username = html_doc.css('h2.ProfileHeaderCard-screenname.u-inlineBlock.u-dir').inner_text.strip
+        @twitter_user.description = html_doc.css('p.ProfileHeaderCard-bio.u-dir').inner_text.strip
+        if @twitter_user.save
+            redirect_to twitter_user_path(@twitter_user), notice: 'User saved successfully'
+        else
+            render :new
+        end
     end
-  
+    
+        def show
+            @twitter_user = TwitterUser.find(params[:id])
+        end
+    
     private
-    def get_users(url)
+    def get_user(url)
         # t_user = TwitterUser.where(name: name).first
         html_file = open(url).read
         # html_file = open(t_user.page_address).read
         html_doc = Nokogiri::HTML(html_file)
-
-        puts html_doc.css('a.ProfileHeaderCard-nameLink.u-textInheritColor.js-nav').inner_text
-        puts html_doc.css('h2.ProfileHeaderCard-screenname.u-inlineBlock.u-dir').inner_text.strip
-        puts html_doc.css('p.ProfileHeaderCard-bio.u-dir').inner_text.strip
+        return html_doc
     end
-
+    
+    def twitter_user_params
+        params.require(:twitter_user).permit(:name, :page_address)
+    end
 end
 
-# get_users('http://www.twitter.com/catracalivre')
+# get_user('http://www.twitter.com/catracalivre')
